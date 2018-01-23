9cbed32b2a40cb08dff5f9acd6a72bff7c7ffb46
Jeff Yang <jeffjieyang@gmail.com>
2018-01-23 13:19:58 -0800

    Add user registration

--- controllers/register-controller.js
+++ controllers/register-controller.js
@@ -1,9 +1,28 @@
-var client = require('./server.js');
+var server = require('./server.js');
 
 module.exports.registerUser = function (request, response) {
-  // TODO register a new user
+
+  server.bcrypt.hash(request.password, 10, function(err, hash) {
+
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      var token = jwt.sign({email: request.email}, 'secretkeyTODO');
+
+      server.db.run("INSERT INTO users (email, pwhash, num, jwt) VALUES (?, ?, ?, ?)", request.email, hash, 0, token);
+
+      res.json({
+        num: 0,
+        token: token
+      });
+    }
+
+  });
+
+
 }
 
-function newSalt() {
-  // TODO generate password salt 
-}
+
+
+
+  // TODO response
